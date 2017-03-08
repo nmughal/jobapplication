7548c93f93cb5e1521c2e7de6354446d2f49da1d
Nadia Mughal <nadiamughal@Nadias-MacBook-Pro.local>
2017-03-08 15:46:40 -0500

    created the function to change the experience range input when the dot is slided to the dot

--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,25 @@
 (function uiapp() {
 
-function blurEvent(eventObj) {
-  if (eventObj.target.value.length === 0) {
-    eventObj.target.parentNode.parentNode.classList.add('has-error');
+  function blurEvent(eventObj) {
+    if (eventObj.target.value.length === 0) {
+      eventObj.target.parentNode.parentNode.classList.add('has-error');
+    }
   }
-}
-document.querySelector('#full-name').addEventListener('blur', blurEvent);
+  document.querySelector('#full-name').addEventListener('blur', blurEvent);
+
+
+// When the range input ("experience") is changed, update the text in the figure
+// with the correct value from the input. So if I slide the dot to the right 1 step
+// the figure should read "6 years".
+
+
+  function changeEvent(eventObj) {
+    console.log(eventObj.target.value);
+    console.log(eventObj.target.parentNode.childNodes);
+    eventObj.target.parentNode.childNodes[3].innerText = eventObj.target.value + ' years';
+  }
+  document.querySelector('#exp').addEventListener('change', changeEvent);
+
 
 
 })(); // executing my IIFE (called uiapp)
